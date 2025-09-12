a8ffa5be496fd69fc3279af827221a0f734f6766
abfabs <albaeftimi@gmail.com>
2025-09-12 10:28:10 +0000

    0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-"""Module that contain function that adds two integers"""
-
 
 def add_integer(a, b=98):
-    if type(a) is not int and type(a) is not float:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
