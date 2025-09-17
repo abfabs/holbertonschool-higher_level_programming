407ec24fca279c16fc4cc9677a85b0784f215caa
abfabs <albaeftimi@gmail.com>
2025-09-17 08:32:22 +0000

    try again

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """creating a rectangle"""
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
