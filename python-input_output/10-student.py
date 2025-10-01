89f5687c8e70fb0c5615d3e8167a2c32d935611e
abfabs <albaeftimi@gmail.com>
2025-10-01 10:31:33 +0000

    pycode

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -6,7 +6,10 @@ first name, last name, and age, and provides a method
 to retrieve a dictionary representation of the instance.
 """
 
+
 class Student:
+    """Defines a student by first name, last name, and age."""
+
     def __init__(self, first_name, last_name, age):
         """Initialize a Student with first name, last name, and age."""
         self.first_name = first_name
@@ -16,9 +19,15 @@ class Student:
     def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of a Student instance.
-        If attrs is a list of strings, only attributes listed in attrs are retrieved.
-        Otherwise, all attributes are retrieved.
+
+        If attrs is a list of strings, only attribute names contained
+        in this list must be retrieved. Otherwise, all attributes
+        are retrieved.
         """
-        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        if (isinstance(attrs, list) and
+                all(isinstance(attr, str) for attr in attrs)):
+            return {
+                attr: getattr(self, attr) for attr in attrs
+                if hasattr(self, attr)
+            }
         return self.__dict__
