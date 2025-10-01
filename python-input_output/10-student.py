615c0331b007653e8a707e0d1f7c8a8c4dff50f7
abfabs <albaeftimi@gmail.com>
2025-10-01 10:27:03 +0000

    student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student with first name, last name, and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        If attrs is a list of strings, only attributes listed in attrs are retrieved.
+        Otherwise, all attributes are retrieved.
+        """
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
